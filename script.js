fac18073f9eed39a2fa7f7ef888d72d094fe7a6f
Ray Zin Ng <s10222457@connect.np.edu.sg>
2022-11-04 00:16:17 +0800

    clean up login/sign up backend done store UI

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "https://www.gstatic.com/firebasejs/9.12.0/firebase-app.js";
-import { getAuth, onAuthStateChanged,  signInWithEmailAndPassword, signOut, createUserWithEmailAndPassword} from "https://www.gstatic.com/firebasejs/9.12.0/firebase-auth.js";
-import { getDatabase, ref, set, onValue, get, child} from "https://www.gstatic.com/firebasejs/9.12.0/firebase-database.js";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.13.0/firebase-app.js";
+import { getAuth, onAuthStateChanged, signInWithEmailAndPassword, signOut, createUserWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/9.13.0/firebase-auth.js";
+import { getDatabase, ref, set, onValue, get, child } from "https://www.gstatic.com/firebasejs/9.13.0/firebase-database.js";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -23,105 +23,105 @@ const app = initializeApp(firebaseConfig);
 
 const db = getDatabase();
 
-const auth = getAuth(app);
+const auth = getAuth();
 onAuthStateChanged(auth, (user) => {
-    var notLoggedIn = document.getElementById("notLoggedIn");
-    var loggedIn = document.getElementById("loggedIn");
+  var notLoggedIn = document.getElementById("notLoggedIn");
+  var loggedIn = document.getElementById("Nameholder");
+  var btn = document.getElementById("btn");
+  var myModal = bootstrap.Modal.getOrCreateInstance(document.getElementById('exampleModal'));
   if (user) {
     // User is signed in, see docs for a list of available properties
     // https://firebase.google.com/docs/reference/js/firebase.User
     const uid = user.uid;
     loggedIn.style.display = "block"
-    notLoggedIn.style.display = "none"
-    if(localStorage.name){
-      document.getElementById("Nameholder").innerHTML = localStorage.getItem("name");
-    }
+    btn.innerText = "Log out"
+    btn.addEventListener("click", () => {
+      signOut(auth).then(() => {
+        // Sign-out successful.
+        myModal.hide();
+      }).catch((error) => {
+        // An error happened.
+      });
+    })
+    //window.location = '/userIndex.html';
+    //notLoggedIn.style.display = "none"
+    returnName();
   } else {
     loggedIn.style.display = "none"
-    notLoggedIn.style.display = "block"
+    btn.innerText = "Login"
+    btn.addEventListener("click", () => {
+      myModal.show();
+    })
   }
 });
 
 var button1 = document.getElementById("loginbutton");
 var button2 = document.getElementById("signupbutton");
-var button3 = document.getElementById("logout");
 var myModal = bootstrap.Modal.getOrCreateInstance(document.getElementById('exampleModal'));
 
-button1.addEventListener("click", (e)=>{
-    e.preventDefault();
-    var email = document.getElementById("exampleInputEmail1").value
-    var password = document.getElementById("exampleInputPassword1").value
-    var errormsg = document.getElementsByClassName("errormsg")
-    signInWithEmailAndPassword(auth, email, password)
+button1.addEventListener("click", (e) => {
+  e.preventDefault();
+  var email = document.getElementById("exampleInputEmail1").value
+  var password = document.getElementById("exampleInputPassword1").value
+  var errormsg = document.getElementsByClassName("errormsg")
+  signInWithEmailAndPassword(auth, email, password)
     .then((userCredential) => {
-        // Signed in 
-        const user = userCredential.user;
-        const inputField = document.getElementById("form");
-        inputField.reset();
-        myModal.hide();
-        returnName();
-        
+      // Signed in 
+      const user = userCredential.user;
+      const inputField = document.getElementById("form");
+      inputField.reset();
+      myModal.hide();
     })
-    
+
     .catch((error) => {
-        const errorCode = error.code;
-        const errorMessage = error.message;
-        alert(errorMessage)
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      alert(errorMessage)
     });
 
 })
 
-button2.addEventListener("click", ()=>{
-    e.preventDefault();
-    var email = document.getElementById("exampleInputEmail1").value
-    var password = document.getElementById("exampleInputPassword1").value
-    var name = document.getElementById("name").value
-    var errormsg = document.getElementsByClassName("errormsg")
-    createUserWithEmailAndPassword(auth, email, password)
-  .then((userCredential) => {
-    var test = document.getElementById("test")
-    const user = userCredential.user;
-    writeUserData(user.uid,name,email);
-    const inputField = document.getElementById("form");
-    inputField.reset();
-    myModal.hide();
-    //var nameholder = document.getElementById("Nameholder");
-    //nameholder.innerText = `${name}`
-  })
-  .catch((error) => {
-    const errorCode = error.code;
-    const errorMessage = error.message;
-    alert(errorMessage);
-  });
-})
-
-button3.addEventListener("click", ()=>{
-    signOut(auth).then(() => {
-    // Sign-out successful.
-    }).catch((error) => {
-    // An error happened.
+button2.addEventListener("click", (e) => {
+  e.preventDefault();
+  var email = document.getElementById("exampleInputEmail1").value
+  var password = document.getElementById("exampleInputPassword1").value
+  var name = document.getElementById("name").value
+  var errormsg = document.getElementsByClassName("errormsg")
+  createUserWithEmailAndPassword(auth, email, password)
+    .then((userCredential) => {
+      var test = document.getElementById("test")
+      const user = userCredential.user;
+      writeUserData(user.uid, name, email);
+      const inputField = document.getElementById("form");
+      inputField.reset();
+      myModal.hide();
+    })
+    .catch((error) => {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      alert(errorMessage);
     });
 })
 
 // Save message to firebase
 function writeUserData(userId, name, email) {
-    const db = getDatabase();
-    set(ref(db, 'users/' + userId), {
-      username: name,
-      email: email,
-    });
-  }
+  const db = getDatabase();
+  set(ref(db, 'users/' + userId), {
+    username: name,
+    email: email,
+  });
+}
 
-  function returnName(){
-    const userId = auth.currentUser.uid;
-    return onValue(ref(db, '/users/' + userId), (snapshot) => {
-      const username = (snapshot.val() && snapshot.val().username) || 'Anonymous';
-      localStorage.setItem = ("name", username);
-      document.getElementById("Nameholder").innerHTML = localStorage.getItem("name");
-    }, {
-      onlyOnce: true
-    });
-  }
+//return user name
+function returnName() {
+  const userId = auth.currentUser.uid;
+  return onValue(ref(db, '/users/' + userId), (snapshot) => {
+    var username = (snapshot.val() && snapshot.val().username) || 'Anonymous';
+    document.getElementById("Nameholder").innerText = username;
+  }, {
+    onlyOnce: true
+  });
+}
 
 
 
@@ -133,18 +133,18 @@ let change2 = document.getElementById("change2");
 
 signup.style.display = "none";
 
-toggle.addEventListener("click", ()=>{
-    if(signup.style.display === "none"){
-        signup.style.display = "block"
-        loginbutton.style.display = "none"
-        change1.innerText = "Already have an account? Click"
-        change2.innerText = "login"
-    }
-    else{
-        signup.style.display = "none"
-        loginbutton.style.display = "block"
-        change1.innerText = "Don't have an account? Click"
-        change2.innerText = "sign up"
-    }
-      
+toggle.addEventListener("click", () => {
+  if (signup.style.display === "none") {
+    signup.style.display = "block"
+    loginbutton.style.display = "none"
+    change1.innerText = "Already have an account? Click"
+    change2.innerText = "login"
+  }
+  else {
+    signup.style.display = "none"
+    loginbutton.style.display = "block"
+    change1.innerText = "Don't have an account? Click"
+    change2.innerText = "sign up"
+  }
+
 })
