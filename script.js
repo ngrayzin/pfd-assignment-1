22601a57740875bd497a30d247e6edc2a3613e7c
Ray Zin Ng <s10222457@connect.np.edu.sg>
2022-11-05 23:24:51 +0800

    done add items into firebase. done halfway add image into firebase

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.13.0/firebase-app.js";
 import { getAuth, onAuthStateChanged, signInWithEmailAndPassword, signOut, createUserWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/9.13.0/firebase-auth.js";
-import { getDatabase, ref, set, onValue, get, child } from "https://www.gstatic.com/firebasejs/9.13.0/firebase-database.js";
+import { getDatabase, ref as ref_database, set, onValue, get, child } from "https://www.gstatic.com/firebasejs/9.13.0/firebase-database.js";
+import { getStorage, ref as ref_storage, uploadBytes, getDownloadURL} from "https://www.gstatic.com/firebasejs/9.13.0/firebase-storage.js";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -22,12 +23,22 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 
 const db = getDatabase();
+const storage = getStorage();
+
+let PATHNAME = "";
+let x = window.location.pathname.split("/");
+PATHNAME = x[1]
+
+console.log(PATHNAME);
+
 
 const auth = getAuth();
 onAuthStateChanged(auth, (user) => {
   var notLoggedIn = document.getElementById("notLoggedIn");
   var loggedIn = document.getElementById("Nameholder");
   var btn = document.getElementById("btn");
+  var donate = document.getElementById("dtn");
+  var home = document.getElementById("hme");
   var myModal = bootstrap.Modal.getOrCreateInstance(document.getElementById('exampleModal'));
   if (user) {
     // User is signed in, see docs for a list of available properties
@@ -43,15 +54,25 @@ onAuthStateChanged(auth, (user) => {
         // An error happened.
       });
     })
-    //window.location = '/userIndex.html';
-    //notLoggedIn.style.display = "none"
+    if(PATHNAME == "index.html"){
+      location.href = "userIndex.html";
+    }
+    home.href = "userIndex.html"
+    donate.href = "donation.html"
     returnName();
   } else {
+    home.href = "index.html"
     loggedIn.style.display = "none"
     btn.innerText = "Login"
     btn.addEventListener("click", () => {
       myModal.show();
-    })
+    });
+    donate.addEventListener("click", () => {
+      myModal.show();
+    });
+    if(PATHNAME == "donation.html" || PATHNAME == "userIndex.html"){
+      location.href = "index.html";
+    }
   }
 });
 
@@ -61,6 +82,7 @@ var myModal = bootstrap.Modal.getOrCreateInstance(document.getElementById('examp
 
 button1.addEventListener("click", (e) => {
   e.preventDefault();
+  const inputField = document.getElementById("form");
   var email = document.getElementById("exampleInputEmail1").value
   var password = document.getElementById("exampleInputPassword1").value
   var errormsg = document.getElementsByClassName("errormsg")
@@ -68,7 +90,6 @@ button1.addEventListener("click", (e) => {
     .then((userCredential) => {
       // Signed in 
       const user = userCredential.user;
-      const inputField = document.getElementById("form");
       inputField.reset();
       myModal.hide();
     })
@@ -76,23 +97,23 @@ button1.addEventListener("click", (e) => {
     .catch((error) => {
       const errorCode = error.code;
       const errorMessage = error.message;
-      alert(errorMessage)
+      alert(errorMessage);
+      inputField.reset();
     });
 
 })
 
 button2.addEventListener("click", (e) => {
   e.preventDefault();
+  const inputField = document.getElementById("form");
   var email = document.getElementById("exampleInputEmail1").value
   var password = document.getElementById("exampleInputPassword1").value
   var name = document.getElementById("name").value
   var errormsg = document.getElementsByClassName("errormsg")
   createUserWithEmailAndPassword(auth, email, password)
     .then((userCredential) => {
-      var test = document.getElementById("test")
       const user = userCredential.user;
-      writeUserData(user.uid, name, email);
-      const inputField = document.getElementById("form");
+      writeUserData(user.uid, name, email)
       inputField.reset();
       myModal.hide();
     })
@@ -100,28 +121,83 @@ button2.addEventListener("click", (e) => {
       const errorCode = error.code;
       const errorMessage = error.message;
       alert(errorMessage);
+      inputField.reset();
     });
 })
 
+if(PATHNAME == "donation.html"){
+    var pname = document.getElementById("productname");
+    const ploc =  document.getElementById("location");
+    const pcond = document.getElementById("condition");
+    var desc = document.getElementById("desc");
+    const image = document.getElementById("img");
+    const submit = document.getElementById("psub");
+    var files= [];
+    submit.addEventListener("click", (e)=> {
+      var user = auth.currentUser;
+      var nname = pname.value;
+      var ddesc = desc.value;
+      var loctext = ploc.options[ploc.selectedIndex].text;
+      var context = pcond.options[pcond.selectedIndex].text;
+      var img = image.value;
+      /*files = e.target.files;
+      reader = new FileReader();
+      reader.onload = function(){
+        document.getElementById("img").src = reader.result;
+      }
+      reader.readAsDataURL(files[0]);*/
+      const storageRef = ref_storage(storage, user.uid + nname + "img");
+      uploadBytes(storageRef, img).then((snapshot) => {
+        console.log('Uploaded a blob or file!');
+      });
+
+      writeProductData(nname, user, loctext, context, ddesc, user.uid + nname)
+    })
+}
+
+
 // Save message to firebase
 function writeUserData(userId, name, email) {
-  const db = getDatabase();
-  set(ref(db, 'users/' + userId), {
+  set(ref_database(db, 'users/' + userId), {
     username: name,
     email: email,
+  });
+}
+
+function writeProductData(name, user, location, condition, desc, img){
+  set(ref_database(db, 'product/' + user.uid + name), {
+    product_name: name,
+    posted_by: user.uid,
+    location : location,
+    condition: condition,
+    description: desc,
+    image: img,
+    claimed : false,
   });
 }
 
 //return user name
 function returnName() {
   const userId = auth.currentUser.uid;
-  return onValue(ref(db, '/users/' + userId), (snapshot) => {
+  return onValue(ref_database(db, '/users/' + userId), (snapshot) => {
     var username = (snapshot.val() && snapshot.val().username) || 'Anonymous';
     document.getElementById("Nameholder").innerText = username;
   }, {
     onlyOnce: true
   });
 }
+
+/*function checkIfLogin(e) {
+  var user = auth.currentUser;
+  if(user){
+    //do nothing
+  }
+  else{
+    e.preventDefault();
+    //window.location.href = "/index.html";
+    myModal.show();
+  }
+}*/
 
 
 
