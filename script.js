ad60947f4d7cbb65a5134e2b41f28989ee153370
Ray Zin Ng <s10222457@connect.np.edu.sg>
2022-11-19 03:23:13 +0800

    pathname testing

--- script.js
+++ script.js
@@ -511,7 +511,7 @@ function change(key,poster){
       //location.reload();
       $('.toast').toast('show');
       //var newpath = window.location.pathname.substr(1) + '?deleteSuccess=1';
-      location.href = "store.html" + '?deleteSuccess=1';
+      window.location.href = "store.html" + '?deleteSuccess=1';
     }
   }
   else{
