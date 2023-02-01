b6e80f668cfdcb160bb2ae4049f2f96cf8a2c2cd
Ray Zin Ng <rayzin147@gmail.com>
2023-02-01 21:31:13 +0800

    done skeleton for faq

--- script.js
+++ script.js
@@ -249,6 +249,23 @@ if (PATHNAME == "donation.html") {
   })
 }
 
+if(PATHNAME == "index.html"){
+  var coll = document.getElementsByClassName("collapsible");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("faqactive");
+    var content = this.nextElementSibling;
+    if (content.style.maxHeight){
+      content.style.maxHeight = null;
+    } else {
+      content.style.maxHeight = content.scrollHeight + "px";
+    } 
+  });
+}
+}
+
 
 if (PATHNAME == "store.html") {
   const urlParams = new URLSearchParams(window.location.search);
